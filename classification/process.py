edc2de0adefe5221d9f1675ebf73c5f2f2fa61a9
alaniszhao <zhaoalanis@gmail.com>
2024-09-21 15:11:31 -0400

    code to rename dataset imgs for processing

---
+++ classification/process.py
@@ -0,0 +1,13 @@
+import os
+
+#renaming images
+'''
+rootdir = '../../../../img/'
+for subdir, dirs, files in os.walk(rootdir):
+    print(subdir)
+    if(subdir=='../../../../img/'):
+        continue
+    for f in files:
+        os.rename(subdir+"/"+f, subdir[16:]+"_"+f)
+'''
+
