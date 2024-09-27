023b647a17ae2db699b5e02c617901cff1799431
alaniszhao <zhaoalanis@gmail.com>
2024-09-26 22:51:38 -0400

    fixed label encoding, changed to conv nn

--- classification/train.py
+++ classification/train.py
@@ -8,14 +8,14 @@ from keras.utils import Sequence
 import sys
 
 np.set_printoptions(threshold=sys.maxsize)
-df = pd.read_csv('fashion-dataset/styles.csv')
+df = pd.read_csv('fashion-dataset/styles.csv', nrows = 1024)
 df = df[df['masterCategory'] == 'Apparel']
 df = df[['id', 'articleType', 'baseColour', 'season', 'usage']]
 df.dropna(inplace=True)
 num_drop = len(df)-(len(df)//32*32)
 df = df[:-num_drop]
 
-df = pd.get_dummies(df, columns=['articleType', 'baseColour', 'season', 'usage'])
+df = pd.get_dummies(df, columns=['articleType', 'baseColour', 'season', 'usage']).astype(int)
 def preprocess_image(image_id, target_size=(135,180)):
     image_path = f'fashion-dataset/images/{image_id}.jpg'
     img = load_img(image_path, target_size=target_size)
@@ -43,6 +43,7 @@ class DataGenerator(Sequence):
 
 X = np.stack(df['id'].values)
 y = df.drop(columns=['id']).values
+print(y)
 
 x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
@@ -50,19 +51,25 @@ train_generator = DataGenerator(x_train, y_train, batch_size=32)
 test_generator = DataGenerator(x_test, y_test, batch_size=32)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(135,180,3)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(135, 180, 3)),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dense(df.shape[1] - 1)
+    tf.keras.layers.Dense(df.shape[1] - 1,activation='sigmoid')
 ])
 
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 model.fit(train_generator, epochs=10, verbose = 2)
 
-test_loss, test_acc = model.evaluate(test_generator, verbose=2)
+#test_loss, test_acc = model.evaluate(test_generator, verbose=2)
 
-print('\nTest accuracy:', test_acc)
+#print('\nTest accuracy:', test_acc)
 
 '''
 tyring to understand what each of these methods are doing to understand
