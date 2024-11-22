01295b905cf315d5bf9f9f573bd665af79f5b612
rkrzywda <50608358+rkrzywda@users.noreply.github.com>
2024-11-22 14:54:20 -0500

    pushed sending changes and merged model changes

--- camera/video_capture.py
+++ camera/video_capture.py
@@ -1,7 +1,7 @@
 #starter code from CodingLikeMad's Reading Webcams in Python [Python OpenCV Tutorial] youtube video
 import cv2
 import sys
-#import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 from fastapi import FastAPI, HTTPException
 import mysql.connector
@@ -41,81 +41,85 @@ color_classes = ['Beige',
 'Yellow'
 ]
 
-# type_model = tf.keras.models.load_model('type_model.keras')
-# colour_model = tf.keras.models.load_model('colour_model.keras')
-# usage_model = tf.keras.models.load_model('usage_model.keras')
+type_model = tf.keras.models.load_model('type_model.keras')
+colour_model = tf.keras.models.load_model('colour_model.keras')
+usage_model = tf.keras.models.load_model('usage_model.keras')
 
 
 take_picture = False
 exit_loop = False
+save_to_database = False
 
 def on_press(key):
-    global take_picture, exit_loop
+    global take_picture, exit_loop, save_to_database
     try:
-        if key.char == 'k':
+        if key.char == 'c':
             take_picture = True
-        elif key.char == 'f':
+        elif key.char == '0':
             exit_loop = True
+        elif key.char == 's':
+            save_to_database = True
     except AttributeError:
         pass
 
 
 def scan_clothing():
-    global take_picture, exit_loop
+    global take_picture, exit_loop, save_to_database
     camera = cv2.VideoCapture(0)
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     camera.set(cv2.CAP_PROP_FPS, 30)
-    print(camera)
+
     if not camera.isOpened():
         print("Error: Could not open camera.")
         exit()
 
     while camera.isOpened():
-        # Capture frame-by-frame
-        # print("should be waiting for key input")
-        if take_picture: #when enter is pressed, take an image after a 5s delay
+        if take_picture: #when enter is pressed, take an image after a 2s delay
+            print()
             time.sleep(2)
-            print("in if statement")
             ret, img = camera.read()
-            img = cv2.resize(img, (224, 224))
             if not ret:
                 print("Error: Failed to capture image.")
                 break
-            print("trying to show the image")
+            print("showing image")
             cv2.imshow('Camera Feed', img)
             cv2.waitKey(2000)
             cv2.destroyAllWindows()
             cv2.waitKey(100)
+            predicted_classes = feedIntoModel(img);
+            print(predicted_classes)
             take_picture = False
-        
-        #predicted_classses = feedIntoModel(img);
-        #sendToDatabase(img, predicted_classes)
 
-        if exit_loop:
-            print("trying to exit loop")
+        
+        if save_to_database: #button that indicates we should save the data has been pressed
+            print()
+            print("sending to database")
+            sendToDatabase(img, "Test Data")
+            save_to_database = False
+        
+        
+        if exit_loop: #Button that we should exit the scanning loop has been sent
+            print()
+            print("exiting loop")
             break
         
-       
-        
         # cv2.imwrite('frame_{}.jpg'.format(frame_number), frame) //saves it to a jpg if we want
-    print("trying to send to database")
-    sendToDatabase("bad value", "another one")
-    print("finished trying")
+
     camera.release()
     cv2.destroyAllWindows()
 
 
-def sendToDatabase(img, predicted_classes): #TODO send to database with img and predicted_clases, probably a post request
-    outfitUrl = "http://0.0.0.0:8000/outfit/info" 
+#Sends a taken image to the database
+def sendToDatabase(img, predicted_classes):
+    outfitUrl = "http://128.2.13.179:8000/outfit/info" 
     #when we run it with --host 0.0.0.0 might need to change it to the hostees ipaddress
     test_outfit_data = {
         "clothingType": "Sweater",
         "color": "Blue",
-        "season": "Winter", 
         "usageType":"Formal",
     }
-    print("trying to send post request")
+    print("sending post request")
     response = requests.post(outfitUrl, json=test_outfit_data) #send post request
     print("Response from API ", response.json())
     return
