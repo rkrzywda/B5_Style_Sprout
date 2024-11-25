c22b6dc857151fa88f2b53cc27cfb39e19ca56bc
rkrzywda <50608358+rkrzywda@users.noreply.github.com>
2024-11-24 19:27:10 -0500

    fixed tensorflow on Jetson and updated model code

--- camera/video_capture.py
+++ camera/video_capture.py
@@ -1,15 +1,12 @@
 #starter code from CodingLikeMad's Reading Webcams in Python [Python OpenCV Tutorial] youtube video
 import cv2
 import sys
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
-from fastapi import FastAPI, HTTPException
-import mysql.connector
-import random
 import time
 import requests
 from pynput import keyboard
-from config import db_config
+import tflite_runtime.interpreter as tflite
  
 type_classes = ['Blazers',
 'Cardigan',
@@ -40,11 +37,24 @@ color_classes = ['Beige',
 'White',
 'Yellow'
 ]
+usage_classes=['casual', 'formal']
 
-type_model = tf.keras.models.load_model('type_model.keras')
-colour_model = tf.keras.models.load_model('colour_model.keras')
-usage_model = tf.keras.models.load_model('usage_model.keras')
+type_model = tflite.Interpreter(model_path='type.tflite')
+type_model.allocate_tensors()
+type_input = type_model.get_input_details()
+type_output = type_model.get_output_details()
 
+color_model = tflite.Interpreter(model_path='color.tflite')
+color_model.allocate_tensors()
+color_input = color_model.get_input_details()
+color_output = color_model.get_output_details()
+
+usage_model = tflite.Interpreter(model_path='usage.tflite')
+usage_model.allocate_tensors()
+usage_input = usage_model.get_input_details()
+usage_output = usage_model.get_output_details()
+
+print("Que pasa??")
 
 take_picture = False
 exit_loop = False
@@ -91,19 +101,19 @@ def scan_clothing():
             print(predicted_classes)
             take_picture = False
 
-        
+       
         if save_to_database: #button that indicates we should save the data has been pressed
             print()
             print("sending to database")
             sendToDatabase(img, "Test Data")
             save_to_database = False
-        
-        
+       
+       
         if exit_loop: #Button that we should exit the scanning loop has been sent
             print()
             print("exiting loop")
             break
-        
+       
         # cv2.imwrite('frame_{}.jpg'.format(frame_number), frame) //saves it to a jpg if we want
 
     camera.release()
@@ -112,7 +122,7 @@ def scan_clothing():
 
 #Sends a taken image to the database
 def sendToDatabase(img, predicted_classes):
-    outfitUrl = "http://128.2.13.179:8000/outfit/info" 
+    outfitUrl = "http://128.2.13.179:8000/outfit/info"
     #when we run it with --host 0.0.0.0 might need to change it to the hostees ipaddress
     test_outfit_data = {
         "clothingType": "Sweater",
@@ -124,43 +134,35 @@ def sendToDatabase(img, predicted_classes):
     print("Response from API ", response.json())
     return
 
-def feedIntoModel(img):
-    # image_resized = cv2.resize(img, (224, 224))
-    # image_normalized = image_resized / 255.0
-    # input_image = np.expand_dims(image_normalized, axis=0)
 
-    # type_predictions = type_model.predict(input_image)
-    # predicted_type = type_classes[np.argmax(type_predictions)]
-    # string indicating the predicted clothing type
-
-    # colour_predictions = colour_model.predict(input_image)
-    # predicted_color = color_classes[np.argmax(color_predictions)]
-    pass
-
-'''
 def feedIntoModel(img):
     image_resized = cv2.resize(img, (224, 224))
     image_normalized = image_resized / 255.0
     input_image = np.expand_dims(image_normalized, axis=0)
+    input_image = input_image.astype(np.float32)
 
-    type_predictions = type_model.predict(input_image)
+    type_model.set_tensor(type_input[0]['index'], input_image)
+    type_model.invoke()
+    type_predictions = type_model.get_tensor(type_output[0]['index'])[0]
     predicted_type = type_classes[np.argmax(type_predictions)]
 
-    color_predictions = colour_model.predict(input_image)
+    color_model.set_tensor(color_input[0]['index'], input_image)
+    color_model.invoke()
+    color_predictions = color_model.get_tensor(color_output[0]['index'])[0]
     predicted_color = color_classes[np.argmax(color_predictions)]
 
     predicted_usage = 'Casual'
     if predicted_type in {'Dresses', 'Skirts', 'Tops', 'Trousers', 'Jackets'}:
-        usage_predictions = usage_model.predict(input_image, verbose=0)[0]
-        predicted_usage = usage_predictions[0]>0.5
+        usage_model.set_tensor(usage_input[0]['index'], input_image)
+        usage_model.invoke()
+        usage_predictions = usage_model.get_tensor(usage_output[0]['index'])[0]
+        predicted_usage = usage_classes[np.argmax(usage_predictions)]
     elif predicted_type in {'Blazers'}:
         predicted_usage = 'Formal'
-'''
-
-#below is where opencv-python is stored on the virtual environemnt capstone_cv
-# /home/style_sprout/Desktop/B5_Style_Sprout/camera/capstone_cv/lib/python3.8/site-packages
-#/usr/lib/python3/dist-packages/cv2.cpython-38-aarch64-linux-gnu.so
-#export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages/cv2.cpython-38-aarch64-linux-gnu.so
+    print(predicted_type)
+    print(predicted_color)
+    print(predicted_usage)
+    return [predicted_type, predicted_color, predicted_usage]
 
 
 def main():
