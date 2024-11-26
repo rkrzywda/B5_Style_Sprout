c5c0cb7d01134d4eed3fe213ea1963ded35f401a
alaniszhao <zhaoalanis@gmail.com>
2024-11-25 23:45:13 -0500

    backend api for updating labels with dummy data

--- backend/main.py
+++ backend/main.py
@@ -408,6 +408,28 @@ def get_closet_images(page: int):
         raise HTTPException(status_code = 400, detail = "Invalid page value")
     return get_image_urls(page)
 
+@app.get("/image_labels/{url}")
+def get_image_labels(url: str):
+    #should return the labels for the url string in order of clothing, color, usage
+    #url is encoded with / as * to prevent url issues
+    url = url.replace('*', '/')
+    return {"labels": ['Dresses', 'Beige', 'Casual']}
+
+@app.post("/label_update/{url}/{column_type}/{new_label}")
+def update_label(url: str, column_type: int, new_label: str):
+    #should update the label for url with the new_label
+    #type denotes which column:
+    #type = 0 means updating clothing type with new_label
+    #type = 1 means updating color with new_label
+    #type = 2 means updating usage with new_label
+    #url is encoded with / as * to prevent url issues
+    print(url)
+    print(column_type)
+    print(new_label)
+    url = url.replace('*', '/')
+    if column_type < 0 or column_type>2:
+        raise HTTPException(status_code = 400, detail = "Invalid column")
+
 # # Mock POST request used by the app to start scanning clothing 
 # @app.post("/start/scanning")
 # def start_scanning():
