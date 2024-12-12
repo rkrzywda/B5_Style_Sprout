295b67732263202018964ffe248e43b734ed7a51
alaniszhao <zhaoalanis@gmail.com>
2024-12-11 19:55:00 -0500

    fix for detection tflite

--- classification/alterative_color.py
+++ classification/alterative_color.py
@@ -2,26 +2,32 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-
+'''
 model_dir = "ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model"
 model = tf.saved_model.load(model_dir)
 infer_fn = model.signatures['serving_default']
+'''
+model = tf.lite.Interpreter(model_path='objectdetect.tflite')
+input_details = model.get_input_details()
+model.resize_tensor_input(input_details[0]['index'], (1, 224, 224, 3))
+model.allocate_tensors()
+output_details = model.get_output_details()
 
 from sklearn.cluster import KMeans
 
 color_classes = {
-    'Beige': [(130, 255), (140, 240), (130, 200)],
-    'Black': [(0, 75), (0, 75), (0, 75)],
-    'Blue': [(0, 150), (0, 150), (150, 255)],
-    'Brown': [(75, 180), (50, 120), (30, 90)],
-    'Green': [(0, 110), (100, 255), (0, 100)],
-    'Grey': [(100, 200), (100, 200), (100, 200)],
+    'Beige': [(150, 200), (145, 200), (135, 200)],
+    'Black': [(0, 60), (0, 60), (0, 60)],
+    'Blue': [(0, 130), (0, 140), (150, 255)],
+    'Brown': [(100, 180), (50, 120), (30, 90)],
+    'Green': [(0, 100), (150, 255), (0, 100)],
     'Orange': [(200, 255), (100, 180), (0, 50)],
-    'Pink': [(200, 255), (150, 220), (200, 255)],
+    'Pink': [(200, 255), (150, 200), (200, 255)],
     'Purple': [(100, 180), (0, 80), (100, 200)],
     'Red': [(150, 255), (0, 100), (0, 100)],
     'White': [(200, 255), (200, 255), (200, 255)],
     'Yellow': [(200, 255), (200, 255), (0, 100)],
+    'Grey': [(100, 200), (100, 200), (100, 200)],
 }
 
 def get_most_prominent_color(image):
@@ -32,7 +38,7 @@ def get_most_prominent_color(image):
     center_x, center_y = w // 2, h // 2
     x, y = np.meshgrid(np.linspace(0, w - 1, w), np.linspace(0, h - 1, h))
 
-    sigma = 0.6 * w
+    sigma = 0.2 * w
     gaussian_weights = np.exp(-((x - center_x)**2 + (y - center_y)**2) / (2 * sigma**2))
     gaussian_weights /= gaussian_weights.sum()
 
@@ -48,31 +54,39 @@ def get_most_prominent_color(image):
 def categorize_color(rgb):
     r, g, b = rgb
 
+    g_ratio = g/r
+    b_ratio = b/r
+
     print(f'{r} {g} {b}')
+    print(f'{g_ratio} {b_ratio}')
 
-    for color, ((r_min, r_max), (g_min, g_max), (b_min, b_max)) in color_classes.items():
-        if r_min <= r <= r_max and g_min <= g <= g_max and b_min <= b <= b_max:
-            return color
-
+    if r>=135 and g_ratio < 0.55 and b_ratio<0.55 and abs(b_ratio-g_ratio)<0.1:
+        return 'Red'
+    if r>135 and g_ratio < 0.85 and b_ratio<0.95 and b_ratio-g_ratio>=0:
+        return 'Pink'
+    if r<65 and g<65 and b <65 and abs(b_ratio-g_ratio)<0.5:
+        return 'Black'
     return 'Unknown'
 
 
 def load_image(image_path):
-    img = tf.io.read_file(image_path)
-    img = tf.image.decode_image(img, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    return img, img.shape
+    #img = tf.io.read_file(image_path)
+    #img = tf.image.decode_image(img, channels=3)
+    #img = tf.image.convert_image_dtype(img, tf.float32)
+    img = cv2.imread(image_path)
+    return img
 
 def predict_color(image_path):
-    image, image_shape = load_image(image_path)
-    image = tf.image.convert_image_dtype(image, dtype=tf.uint8)
+    image = load_image(image_path)
+    image = cv2.resize(image, (224, 224))
 
-    input_tensor = tf.expand_dims(image, axis=0)
+    input_tensor = np.expand_dims(image, axis=0).astype(np.uint8)
+    model.set_tensor(input_details[0]['index'], input_tensor)
+    model.invoke()
 
-    detections = infer_fn(input_tensor)
 
-    boxes = detections['detection_boxes'][0].numpy()
-    scores = detections['detection_scores'][0].numpy()
+    boxes = model.get_tensor(output_details[4]['index'])[0]
+    scores = model.get_tensor(output_details[2]['index'])
     max_score_idx = np.argmax(scores)
 
     highest_score_box = boxes[max_score_idx]
@@ -81,7 +95,7 @@ def predict_color(image_path):
     height, width, _ = image.shape
     top, left, bottom, right = int(ymin * height), int(xmin * width), int(ymax * height), int(xmax * width)
     cropped_image = image[top:bottom, left:right]
-    cropped_image = cropped_image.numpy()
+    cropped_image = cropped_image
     cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR)
     most_prominent_color = get_most_prominent_color(cropped_image)
 
@@ -94,5 +108,6 @@ def predict_color(image_path):
     plt.imshow(color_patch)
     plt.title(f"{categorize_color(most_prominent_color)}")
     plt.show()
+
 
 
